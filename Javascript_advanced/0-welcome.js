d76b3d1fb54273c4ee4514b5f1541d518c602d9d
Hugo Chilemme <hugo.chilemme@holbertonschool.com>
2024-12-19 10:06:52 +0000

    Creating welcome function

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,25 @@
+
+
+
+function welcome(firstName, lastName)
+{
+    if (!firstName) 
+        throw new Error("Missing firstName parameter");
+    if (!lastName) 
+        throw new Error("Missing lasstName parameter");
+    
+    const fullName = `${firstName} ${lastName}`;
+
+    function displayFullName()
+    {
+        alert(`Welcome ${fullName} !`);
+    }
+
+    displayFullName();
+}
+
+
+function displayFullName()
+{
+    alert(`Welcome ${welcome}`)
+}
