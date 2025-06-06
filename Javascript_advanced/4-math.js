2564e297412d6a30206fb620210a65052b47be3b
Hugo Chilemme <hugo.chilemme@holbertonschool.com>
2025-06-06 08:20:52 +0000

    Ajouter les fonctions de calcul pour l'addition et la division

---
+++ Javascript_advanced/4-math.js
@@ -0,0 +1,7 @@
+const divideBy = (firstNumber, secondNumber) => firstNumber / secondNumber;
+const addBy = (firstNumber, secondNumber) => firstNumber + secondNumber;
+
+const addBy100 = (num) => addBy(num, 100);
+const addBy1000 = (num) => addBy(num, 1000);
+const divideBy10 = (num) => divideBy(num, 10);
+const divideBy100 = (num) => divideBy(num, 100);
